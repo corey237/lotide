3dc8b35ee38494bc05cef98774794fa12d6d04ad
Corey Silver <coreysilver@outlook.com>
2023-01-11 14:42:57 -0500

    completed the countLetters function for the lotide project

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❗❗❗ Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const results = {};
+  for (const letter of sentence.toLowerCase()) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
+const test = countLetters("lighthouse in the house");
+assertEqual(test["t"], 2);
+assertEqual(test["s"], 2);
+assertEqual(test["n"], 1);
+assertEqual(test["q"], undefined);
+``;
